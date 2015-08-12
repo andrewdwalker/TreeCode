690e8b8b43e8fe46cf54d40af9557cb610ef8315
andrewdwalker <andrewdwalker@ymail.com>
2015-08-12 10:52:34 -0700

    Code For Binary Trees
    Various classes for binary trees w/ examples of various operations

---
+++ TreeCode/BinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeCode
+{
+    public class BinaryTree
+    {
+        Node<int> _parent = null;
+
+        public bool AddValue(int value)
+        {
+            if (_parent == null)
+            {
+                _parent = new Node<int>();
+                _parent.NodeValue = value;
+            }
+            else
+            {
+                Node<int> temp = _parent;
+                while (temp.Left != null)
+                {
+                    temp = temp.Left;
+                }
+
+                temp.Left = new Node<int>(value);
+
+            }
+
+        }
+    }
+}
---
+++ TreeCode/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeCode
+{
+    public class Node<T>
+    {
+       
+      public  Node() {}
+       public Node(T value)
+    {
+        _nodeValue = value;
+    }
+        T _nodeValue;
+
+        public T NodeValue
+        {
+            get { return _nodeValue; }
+            set { _nodeValue = value; }
+        }
+        Node<T> _left = null;
+        Node<T> _right = null;
+        Node<T> _parent = null;
+
+        public Node<T> Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        public Node<T> Left
+        {
+            get { return _left; }
+            set { _left = value;
+            _left.Parent = this;
+            }
+        }
+        
+        public Node<T> Right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+    }
+}
